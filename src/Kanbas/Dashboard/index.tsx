1e481be5870011c06ee15c58ce44e11d72399dc9
garrettmichiels <michielsgarrett@gmail.com>
2024-03-19 17:43:36 -0400

    modules next

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -1,58 +1,113 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import * as db from "../Database";
-function Dashboard({ courses, course, setCourse, addNewCourse,
-  deleteCourse, updateCourse }: {
-  courses: any[]; course: any; setCourse: (course: any) => void;
-  addNewCourse: () => void; deleteCourse: (course: any) => void;
-  updateCourse: () => void; })
- {
+function Dashboard({
+  courses,
+  course,
+  setCourse,
+  addNewCourse,
+  deleteCourse,
+  updateCourse,
+}: {
+  courses: any[];
+  course: any;
+  setCourse: (course: any) => void;
+  addNewCourse: () => void;
+  deleteCourse: (course: any) => void;
+  updateCourse: () => void;
+}) {
   return (
     <div className="p-4">
-      <h1>Dashboard</h1>              <hr />
+      <h1>Dashboard</h1> <hr />
       <h2>Published Courses (12)</h2> <hr />
       <h5>Course</h5>
-      <input value={course.name} className="form-control"
-             onChange={(e) => setCourse({ ...course, name: e.target.value }) } />
-      <input value={course.number} className="form-control"
-             onChange={(e) => setCourse({ ...course, number: e.target.value }) } />
-      <input value={course.startDate} className="form-control" type="date"
-             onChange={(e) => setCourse({ ...course, startDate: e.target.value }) }/>
-      <input value={course.endDate} className="form-control" type="date"
-             onChange={(e) => setCourse({ ...course, endDate: e.target.value }) } />
-      <button type="button" className="btn btn-success" style={{"border": 'none', "color": 'black' }} 
-      onClick={addNewCourse} >
+      <input
+        value={course.name}
+        className="form-control"
+        onChange={(e) => setCourse({ ...course, name: e.target.value })}
+      />
+      <input
+        value={course.number}
+        className="form-control"
+        onChange={(e) => setCourse({ ...course, number: e.target.value })}
+      />
+      <input
+        value={course.startDate}
+        className="form-control"
+        type="date"
+        onChange={(e) => setCourse({ ...course, startDate: e.target.value })}
+      />
+      <input
+        value={course.endDate}
+        className="form-control"
+        type="date"
+        onChange={(e) => setCourse({ ...course, endDate: e.target.value })}
+      />
+      <button
+        type="button"
+        className="btn btn-success"
+        style={{ border: "none", color: "black" }}
+        onClick={addNewCourse}
+      >
         Add
       </button>
-      <button  type="button" className="btn btn-danger" style={{"border": 'none', "color": 'black' }} 
-      onClick={(event) => {
-                        event.preventDefault();
-                        deleteCourse(course._id);
-                      }}>
-                      Delete
-              </button>
-      <button  type="button" className="btn btn-primary" style={{"border": 'none', "color": 'black' }}
-      onClick={(event) => {
-                event.preventDefault();
-                setCourse(course);
-              }}>
-              Edit
-      </button>
-
+      <button
+        type="button"
+        className="btn btn-primary"
+        style={{ border: "none", color: "black" }} onClick={updateCourse}>Update</button>
       <div className="row">
         <div className="row row-cols-1 row-cols-md-5 g-4">
           {courses.map((course) => (
             <div key={course._id} className="col" style={{ width: 300 }}>
               <div className="card">
-                <img src={`/images/${course.image}`} className="card-img-top"
-                     style={{ height: 150 }}/>
+                <img
+                  src={`/images/${course.image}`}
+                  className="card-img-top"
+                  style={{ height: 150 }}
+                />
                 <div className="card-body">
-                  <Link className="card-title" to={`/Kanbas/Courses/${course._id}/Home`}
-                    style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}>
-                    {course.name} </Link>
+                  <Link
+                    className="card-title"
+                    to={`/Kanbas/Courses/${course._id}/Home`}
+                    style={{
+                      textDecoration: "none",
+                      color: "navy",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    {course.name}
+                    <br />
+                    <button
+                      type="button"
+                      className="btn btn-primary"
+                      style={{ border: "none", color: "black" }}
+                      onClick={(event) => {
+                        event.preventDefault();
+                        setCourse(course);
+                      }}
+                    >
+                      Edit
+                    </button>
+                    <button
+                      type="button"
+                      className="btn btn-danger"
+                      style={{ border: "none", color: "black" }}
+                      onClick={(event) => {
+                        event.preventDefault();
+                        deleteCourse(course._id);
+                      }}
+                    >
+                      Delete
+                    </button>
+                  </Link>
+
                   <p className="card-text">{course.number}</p>
-                  <Link to={`/Kanbas/Courses/${course._id}/Home`} className="btn btn-primary">
-                    Go </Link>
+                  <Link
+                    to={`/Kanbas/Courses/${course._id}/Home`}
+                    className="btn btn-primary"
+                  >
+                    Go{" "}
+                  </Link>
                 </div>
               </div>
             </div>
--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -45,8 +45,7 @@ function Kanbas() {
           addNewCourse={addNewCourse}
           deleteCourse={deleteCourse}
           updateCourse={updateCourse}/>} />
-          <Route path="Courses/:courseId/*" element={  <Courses courses={courses} />} />
-
+          <Route path="Courses/:courseId/*" element={ <Courses courses={courses} />} />
         </Routes>
 
       </div>
